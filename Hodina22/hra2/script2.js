95b9745c0cf42c1e3c2cf122ba6e003f4947c38b
Vojtěch Parma <parma0vojtech@gmail.com>
2026-05-25 17:45:14 +0200

    x
    x

---
+++ Hodina22/hra2/script2.js
@@ -0,0 +1,68 @@
+let grid = document.getElementById("grid");
+let barva = "white";
+
+let text = document.createElement("h2");
+text.innerText = "Mode: white";
+
+document.body.appendChild(text);
+
+document.onkeydown = function(event) {
+
+    if (event.key == "o") {
+        barva = "orange";
+        text.innerText = "Mode: orange";
+    }
+
+    if (event.key == "b") {
+        barva = "blue";
+        text.innerText = "Mode: blue";
+    }
+
+    if (event.key == "r") {
+        barva = "red";
+        text.innerText = "Mode: red";
+    }
+    if (event.key == "g") {
+        barva = "green";
+        text.innerText = "Mode: green";
+    }
+    if (event.key == "w") {
+        barva = "white";
+        text.innerText = "Mode: white";
+    }
+
+};
+document.getElementById("clear").onclick = function() {
+
+    localStorage.removeItem("grid");
+
+    location.reload();
+
+};
+
+// načtení uložených dat
+let ulozeneBarvy = JSON.parse(localStorage.getItem("grid")) || [];
+
+for (let i = 0; i < 100; i++) {
+
+    let box = document.createElement("div");
+
+    box.classList.add("box");
+
+    // načte uloženou barvu
+    if (ulozeneBarvy[i]) {
+        box.style.backgroundColor = ulozeneBarvy[i];
+    }
+
+    box.onclick = function() {
+
+        box.style.backgroundColor = barva;
+
+        // uloží barvu
+        ulozeneBarvy[i] = barva;
+
+        localStorage.setItem("grid", JSON.stringify(ulozeneBarvy));
+    };
+
+    grid.appendChild(box);
+}
---
+++ Hodina22/hra3/script.js
@@ -0,0 +1,84 @@
+let grid = document.getElementById("grid");
+let barva = "white";
+
+let text = document.createElement("h2");
+text.innerText = "Mode: white";
+
+document.body.appendChild(text);
+
+document.onkeydown = function(event) {
+
+    if (event.key == "o") {
+        barva = "orange";
+        text.innerText = "Mode: orange";
+    }
+
+    if (event.key == "b") {
+        barva = "blue";
+        text.innerText = "Mode: blue";
+    }
+
+    if (event.key == "r") {
+        barva = "red";
+        text.innerText = "Mode: red";
+    }
+    if (event.key == "g") {
+        barva = "green";
+        text.innerText = "Mode: green";
+    }
+    if (event.key == "w") {
+        barva = "white";
+        text.innerText = "Mode: white";
+    }
+
+};
+document.getElementById("clear").onclick = function() {
+
+    localStorage.removeItem("grid");
+
+    location.reload();
+
+};
+
+// načtení uložených dat
+let ulozeneBarvy = JSON.parse(localStorage.getItem("grid")) || [];
+
+for (let i = 0; i < 100; i++) {
+
+    let box = document.createElement("div");
+
+    box.classList.add("box");
+
+    // načte uloženou barvu
+    if (ulozeneBarvy[i]) {
+        box.style.backgroundColor = ulozeneBarvy[i];
+    }
+
+    box.onclick = function() {
+
+        box.style.backgroundColor = barva;
+
+        // uloží barvu
+        ulozeneBarvy[i] = barva;
+
+        localStorage.setItem("grid", JSON.stringify(ulozeneBarvy));
+    };
+
+    grid.appendChild(box);
+}
+
+document.getElementById("export").onclick = function() {
+
+    html2canvas(document.getElementById("grid")).then(function(canvas) {
+
+        let link = document.createElement("a");
+
+        link.download = "obrazek.png";
+
+        link.href = canvas.toDataURL();
+
+        link.click();
+
+    });
+
+};
